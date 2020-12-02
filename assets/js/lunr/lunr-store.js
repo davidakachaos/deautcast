15c45b337ad15a73aca4dea72c7b9081b3b7bee7
David Westerink <davidakachaos@gmail.com>
2020-12-02 10:55:28 +0100

    Latest version of De AutCast -

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,25 +1,25 @@
 var store = [{
         "title": "De week van #Gaut",
         "excerpt":"Vandaag, op de wereld autisme dag 2020, is het zo ver! Mijn eerste aflevering van de AutCast is online gekomen. In het kader van de autisme week had ik het idee om de podcast te lanceren. En vandaag leek mij het uitgelezen moment. Deze eerste aflevering is bedoeld als een...","categories": [],
-        "tags": [],
+        "tags": ["persoonlijk"],
         "url": "https://autcast.myautisticself.nl/de-week-van-gaut/",
         "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
       },{
         "title": "Feiten, fabels en vaccinaties",
         "excerpt":"In deze tweede aflevering van de AutCast wil ik het met jullie hebben over feiten, fabels en vaccinaties. Ik behandel verschillende fabels die de ronde doen over autisme, en wat feiten als tegen gewicht. Uiteraard zijn deze feiten en fabels vanuit mezelf besproken. Heb je een opmerking, vraag of andere...","categories": [],
-        "tags": [],
+        "tags": ["persoonlijk"],
         "url": "https://autcast.myautisticself.nl/feiten-fabels-en-vaccinaties/",
         "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
       },{
         "title": "De reis naar mijn diagnose",
         "excerpt":"Na een lang hiaat ben ik terug! Terug met een speciale aflevering, namelijk met mijn webinar wat ik op 26 augustus 2020 heb gegeven in samenwerking met het Autisme Netwerk Friesland. Ik denk dat dit intressant is voor jullie om wat meer over mij te weten te komen. Er is...","categories": [],
-        "tags": [],
+        "tags": ["webinar","persoonlijk"],
         "url": "https://autcast.myautisticself.nl/de-reis-naar-mijn-diagnose/",
         "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
       },{
         "title": "Interview met Hidde Dijkstra",
         "excerpt":"In deze aflevering heb ik een interview met Hidde Dijkstra, een coach bij Scauting in Tjallebert. Hidde is iemand die zelf autistisch is, net als ik, en werkzaam is bij de dagbesteding waar ik zelf kom. Daar heb ik hem leren kennen en heb hem gevraagd om eens met mij...","categories": [],
-        "tags": [],
+        "tags": ["interview","scauting"],
         "url": "https://autcast.myautisticself.nl/interview-met-hidde-dijkstra/",
         "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
       }]
--- deploy.sh
+++ deploy.sh
@@ -50,8 +50,8 @@ if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
   git gc --prune
   git push || exit 1
   git checkout source
-  echo 'https://autcast.nl has been deployed.'
-  notify-send 'https://aucast.nl has been deployed.'
+  echo 'https://autcast.myautisticself.nl has been deployed.'
+  notify-send 'https://aucast.myautisticself.nl has been deployed.'
 else
   notify-send 'Nothing was changed! Aborting deployment.'
   git checkout source
