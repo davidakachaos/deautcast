e17bad8b3714d7bc12055cf7d49ea76aba29c213
David Westerink <davidakachaos@gmail.com>
2022-02-15 09:43:08 +0100

    Latest version of De AutCast - 2022-02-15 09:42

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,72 +2,72 @@ var store = [{
         "title": "De week van #Gaut",
         "excerpt":"Vandaag, op de wereld autisme dag 2020, is het zo ver! Mijn eerste aflevering van de AutCast is online gekomen. In het kader van de autisme week had ik het idee om de podcast te lanceren. En vandaag leek mij het uitgelezen moment. Deze eerste aflevering is bedoeld als een...","categories": [],
         "tags": ["persoonlijk"],
-        "url": "https://autcast.myautisticself.nl/de-week-van-gaut/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/de-week-van-gaut/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "Feiten, fabels en vaccinaties",
         "excerpt":"In deze tweede aflevering van de AutCast wil ik het met jullie hebben over feiten, fabels en vaccinaties. Ik behandel verschillende fabels die de ronde doen over autisme, en wat feiten als tegen gewicht. Uiteraard zijn deze feiten en fabels vanuit mezelf besproken. Heb je een opmerking, vraag of andere...","categories": [],
         "tags": ["persoonlijk"],
-        "url": "https://autcast.myautisticself.nl/feiten-fabels-en-vaccinaties/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/feiten-fabels-en-vaccinaties/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "De reis naar mijn diagnose",
         "excerpt":"Na een lang hiaat ben ik terug! Terug met een speciale aflevering, namelijk met mijn webinar wat ik op 26 augustus 2020 heb gegeven in samenwerking met het Autisme Netwerk Friesland. Ik denk dat dit intressant is voor jullie om wat meer over mij te weten te komen. Er is...","categories": [],
         "tags": ["webinar","persoonlijk"],
-        "url": "https://autcast.myautisticself.nl/de-reis-naar-mijn-diagnose/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/de-reis-naar-mijn-diagnose/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "Interview met Hidde Dijkstra",
         "excerpt":"In deze aflevering heb ik een interview met Hidde Dijkstra, een coach bij Scauting in Tjallebert. Hidde is iemand die zelf autistisch is, net als ik, en werkzaam is bij de dagbesteding waar ik zelf kom. Daar heb ik hem leren kennen en heb hem gevraagd om eens met mij...","categories": [],
         "tags": ["interview","scauting"],
-        "url": "https://autcast.myautisticself.nl/interview-met-hidde-dijkstra/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/interview-met-hidde-dijkstra/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "Interview met Julian Harmens, autistisch dichter",
         "excerpt":"Vandaag ga ik in gesprek met Julian Harmens. Hij is dichter, schrijver en autistisch. Na een lezing die hij gaf met zijn vader (Erik Jan Harmens) heb ik hem uitgenodigd voor een gesprek op de podcast. Hier gaan we in op autisme, dichten en zijn kijk op de wereld. De...","categories": [],
         "tags": ["interview"],
-        "url": "https://autcast.myautisticself.nl/interview-met-julian-harmens-autistisch-dichter/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/interview-met-julian-harmens-autistisch-dichter/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "In gesprek met Erik Velstra van Slimmerik",
         "excerpt":"Slimmerik ondersteuning is een Friese organisatie die zich richt op hulp en ondersteuning van autistische jongeren in Friesland. Directeur en oprichter Erik Velstra is in deze aflevering mijn gast. We gaan in gesprek over Slimmerik, het hoe en waarom van de organisatie en wat voor hulp Slimmerik een jongere met...","categories": [],
         "tags": ["interview","slimmerik"],
-        "url": "https://autcast.myautisticself.nl/in-gesprek-met-erik-velstra-van-slimmerik/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/in-gesprek-met-erik-velstra-van-slimmerik/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "In gesprek met Jolijn de Boer",
         "excerpt":"Vandaag ga ik in gesprek met Jolijn de Boer van het Autisme Netwerk Friesland (ANF). We praten met elkaar over het netwerk en wat het betekend voor ons autisten om een dergelijk netwerk te hebben. Binnenkort, maandagavond 8 februari 2021 om 19.45u, organiseert het netwerk ook een webinar over autisme...","categories": [],
         "tags": ["interview","ANF","autisme","netwerk","Friesland"],
-        "url": "https://autcast.myautisticself.nl/in-gesprek-met-jolijn-de-boer/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/in-gesprek-met-jolijn-de-boer/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "In gesprek met Anna Soorsma van JP van den Bent",
         "excerpt":"In deze aflevering praat ik met Anna Soorsma van de JP van den Bent stichting. We praten over het begeleiden van autisten door de JP van den Bent stichting en hoe Anna zelf ooit begonnen is met dit werk. Meer informatie over JP van den Bent vind je op hun...","categories": [],
         "tags": ["interview","jpvdbent","autisme"],
-        "url": "https://autcast.myautisticself.nl/in-gesprek-met-anna-soorsma-van-jp-van-den-bent/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/in-gesprek-met-anna-soorsma-van-jp-van-den-bent/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "In gesprek met Rosalie Ekstein, autisme socioloog",
         "excerpt":"Vandaag ga ik in gesprek met Rosalie Ekstein, autisme socioloog. Naast diens werk als socioloog, heeft die ook een Facebook pagina Autivisme en de groep Autivisten opgezet. Autivisme is een grote bron van allerlei nuttige informatie over neurodiversiteit in Nederland. De groep is opgezet door autisten voor iedereen die meer...","categories": [],
         "tags": ["autisme","socioloog","interview"],
-        "url": "https://autcast.myautisticself.nl/in-gesprek-met-rosalie-ekstein-autisme-socioloog/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/in-gesprek-met-rosalie-ekstein-autisme-socioloog/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "In gesprek met Horizon Begeleiding - Autisme op school",
         "excerpt":"Vandaag ga ik in gesprek met Lieuwkje Boomsma en Wypkje Wijngaarden van Horizon Begeleiding. Samen praten we over naar school gaan met een “rugzak” en wat te doen als naar school gaan er niet in zit. Wat kunnen we dan doen om jongeren op te vangen en (uiteindelijk) toch weer...","categories": [],
         "tags": ["autisme","begeleiding","netwerk"],
-        "url": "https://autcast.myautisticself.nl/in-gesprek-met-horizon-begeleiding-autisme-op-school/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/in-gesprek-met-horizon-begeleiding-autisme-op-school/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "Naar school in een prikkelende wereld",
         "excerpt":"Deze bijzondere aflevering van de AutCast is gemaakt in samenwerking met het Autisme Netwerk Friesland (ANF). Het ANF is een project gestart om prikkelverwerking op de middelbare school onder de aandacht te brengen bij docenten en ondersteunend personeel. In het kader hiervan worden op verschillende middelbare scholen in Friesland zogenaamde...","categories": [],
         "tags": ["ANF","autisme","school","prikkels","prikkelverwerking"],
-        "url": "https://autcast.myautisticself.nl/naar-school-in-een-prikkelende-wereld/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/naar-school-in-een-prikkelende-wereld/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       },{
         "title": "Autisme en jobcoaching - In gesprek met Willemijn van Dennebroek",
         "excerpt":"In deze aflevering ga ik in gesprek met Willemijn van Dennebroek, job coach bij Scauting. Met haar praat ik over wat een jobcoach eigenlijk is, over werk en wat daarvoor nodig is. Contact met mij opnemen kan via autcast@myautisticself.nl of je kunt een voicemail bericht voor me achter laten. En...","categories": [],
         "tags": ["autisme","jobcoaching","werk","interview"],
-        "url": "https://autcast.myautisticself.nl/autisme-en-jobcoaching-in-gesprek-met-willemijn-van-dennebroek/",
-        "teaser": "https://autcast.myautisticself.nl/assets/img/autcast2.png"
+        "url": "https://autcast.nl/autisme-en-jobcoaching-in-gesprek-met-willemijn-van-dennebroek/",
+        "teaser": "https://autcast.nl/assets/img/autcast2.png"
       }]
